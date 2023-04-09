bc0bca128fafab7a99f24cef7fff66fb77df4061
Denis Floare <floredenis907@yahoo.com>
2023-04-09 19:44:44 +0300

    last_update

---
+++ src/components/Header.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Header() {
+
+
+
+
+    
+  return (
+    <>
+      <State/>
+      <h1></h1>
+      <StopWatch/>
+      </>
+  )
+}
+
+export default Header
---
+++ src/components/Home.tsx
@@ -0,0 +1,51 @@
+
+import React, { useState } from 'react'
+import Player from '../models/Player'
+function Home() {
+
+
+    const [players,setPlayers]=useState([
+        {
+        id:1,
+        nume:"Flore",
+        score:0
+        } ,
+        
+        {
+          id:2,
+          nume:"Bogdan",
+          score:0
+        },
+
+        {
+            id:3,
+            nume:"Marian",
+            score:0
+
+        },
+
+        {
+            id:4,
+            nume:"Maria",
+            score:0
+
+        },
+
+        {
+            id:5,
+            nume:"George",
+            score:0
+
+        }
+    ])
+
+
+
+  return (
+
+    <>
+
+
+</>    
+  )
+}
