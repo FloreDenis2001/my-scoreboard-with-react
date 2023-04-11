ee8ceeea6aac4800968c3cdbf63f943632d92d60
Denis Floare <floredenis907@yahoo.com>
2023-04-11 03:02:10 +0300

    last_update

--- src/components/PlayerComp.tsx
+++ src/components/PlayerComp.tsx
@@ -1,8 +1,7 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Player from '../models/Player'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { IconProp } from '@fortawesome/fontawesome-svg-core';
-import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import { faCrown, faTrash } from '@fortawesome/free-solid-svg-icons'
 
 interface PlayerProps{
     player:Player,
@@ -14,6 +13,7 @@ const PlayerComp:React.FC<PlayerProps>=({player,changePoints,deletePlayer})=>{
 
 
     const[isHovering,setIsHovering]=useState(false);
+    const[iswinner,setWinner]=useState(false);
     
     const handleMouseOver=()=>{
         setIsHovering(true);
@@ -24,26 +24,52 @@ const PlayerComp:React.FC<PlayerProps>=({player,changePoints,deletePlayer})=>{
        
     }
 
+    function minusValidation(){
+        if(player.score>0)
+            changePoints(player.id,-1);
+    }
+
+    function addValidation(){
+        if(player.score<10){
+            changePoints(player.id,1);
+            }
+    }
+
+ 
+    useEffect(()=>{
+       if(player.score===10){
+        setWinner(true);
+       }else {
+        setWinner(false);
+       }
+
+    },[player.score]);
+
 
   return (
     <div className="player id-1">
     <div className="player-details">
+
     {isHovering &&
     <FontAwesomeIcon className='trash-icon' onClick={()=>deletePlayer(player)} icon={faTrash}bounce />
+    }
+
+    {iswinner &&
+    <FontAwesomeIcon className='crown-icon' onClick={()=>deletePlayer(player)} icon={faCrown}bounce />
     }
         <h3 onMouseOver={handleMouseOver} onMouseLeave={handleMouseLeave} className="name">{player.nume}</h3>
     </div>
   
         <div className="player-score">
 
-            <div className="minus" onClick={()=>{changePoints(player.id,-1)}}>
+            <div className="minus" onClick={minusValidation}>
                <p>-</p> 
             </div>
 
             <div className='scores'>
             <h3>{player.score}</h3>
             </div>
-            <div className="plus" onClick={()=>{changePoints(player.id,1)}}>
+            <div className="plus" onClick={addValidation}>
                 <p>+</p>
             </div>
         </div>
